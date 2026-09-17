066a8c80c067c9e71a05e486ac9c0c3efd3524da
Kim Rundgren <kim.rundgren@gmail.com>
2026-09-17 15:00:49 +0200

    display book score and status

--- src/modules/books/renderBook.js
+++ src/modules/books/renderBook.js
@@ -1,4 +1,40 @@
+function renderScore(score) {
+	if (score === 0) {
+		return "☆☆☆☆☆";
+	}
+
+	if (score >= 1 && score <= 5) {
+		return "★".repeat(score) + "☆".repeat(5 - score);
+	}
+
+	return "Not rated";
+}
+
+function renderStatus(isRead) {
+	let statusString;
+	let statusClass;
+
+	if (isRead) {
+		statusString = "Read";
+		statusClass = "read";
+	} else {
+		statusString = "Not read";
+		statusClass = "not-read";
+	}
+
+	return {
+		statusString,
+		statusClass
+	};
+}
+
 export function renderBook(book) {
+	const score = book.getScore();
+	const scoreDisplay = renderScore(score);
+
+	const isRead = book.getIsRead();
+	const status = renderStatus(isRead);
+
 	const bookList = document.querySelector("#bookList");
 
 	const li = document.createElement("li");
@@ -11,10 +47,8 @@ export function renderBook(book) {
 				alt="Book cover of ${book.getTitle()} by ${book.getAuthor()}" 
 				height="50" 
 				width="auto"
-			>
-			<div>
-				${book.getTitle()} <span>${book.getYear()}</span>
-			</div>
+			><div>
+			${book.getTitle()} <span>${book.getYear()}</span></div>
 		</div>
 
 		<div class="col-3">
@@ -22,11 +56,13 @@ export function renderBook(book) {
 		</div>
 
 		<div class="col-2">
-			<span class="stars"></span>
+			<span class="stars">${scoreDisplay}</span>
 		</div>
 
 		<div class="col-2">
-			<span class="badge badge-status"></span>
+			<span class="badge badge-status ${status.statusClass}">
+			${status.statusString}
+			</span>
 		</div>
 
 		<div class="col-2 text-end">
