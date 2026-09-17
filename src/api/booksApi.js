058708b3886035f4b9a9d71dfe45a871607d1847
Kim Rundgren <kim.rundgren@gmail.com>
2026-09-17 15:10:31 +0200

    feat: add POST request for new book

--- src/api/booksApi.js
+++ src/api/booksApi.js
@@ -17,3 +17,38 @@ export async function getBooks() {
 		throw error;
 	}
 }
+
+export async function addBook(newBook) {
+	const url = `${BASE_URL}.json`;
+
+	const book = {
+		title: newBook.title,
+		author: newBook.author,
+		year: newBook.year,
+		cover: newBook.cover,
+		score: null,
+		isRead: false
+	};
+
+	const options = {
+		method: "POST",
+		body: JSON.stringify(book),
+		headers: {
+			"Content-type": "application/json"
+		}
+	}
+
+	try {
+		const response = await fetch(url, options);
+
+		if (!response.ok) {
+			throw new Error("Could not add book");
+		}
+
+		const data = await response.json();
+
+		return data;
+	} catch (error) {
+		throw error;
+	}
+}
