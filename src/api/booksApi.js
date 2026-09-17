ab1957acb246735bf17cb855f660103c996c6855
Kim Rundgren <kim.rundgren@gmail.com>
2026-09-17 14:47:31 +0200

    feat: add GET request for books

--- src/api/booksApi.js
+++ src/api/booksApi.js
@@ -1,2 +1,19 @@
 const BASE_URL = "https://goodreads-kim-rundgren-default-rtdb.europe-west1.firebasedatabase.app/books";
 
+export async function getBooks() {
+	const url = `${BASE_URL}.json`;
+
+	try {
+		const response = await fetch(url);
+
+		if (!response.ok) {
+			throw new Error("Could not add book");
+		}
+
+		const data = await response.json();
+
+		return data;
+	} catch (error) {
+		throw error;
+	}
+}
