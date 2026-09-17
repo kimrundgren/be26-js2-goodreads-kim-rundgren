ae1c006f930429ad1d09eb537a553c5b14052fc3
Kim Rundgren <kim.rundgren@gmail.com>
2026-09-17 14:50:02 +0200

    feat: add Book class

---
+++ src/modules/books/Book.js
@@ -0,0 +1,43 @@
+export class Book {
+	#id;
+	#title;
+	#author;
+	#year;
+	#cover;
+	#score;
+	#isRead;
+
+	constructor(id, title, author, year, cover, score, isRead) {
+		this.#id = id;
+		this.#title = title;
+		this.#author = author;
+		this.#year = year;
+		this.#cover = cover;
+		this.#score = score;
+		this.#isRead = isRead;
+	}
+
+	getTitle() {
+		return this.#title;
+	}
+
+	getAuthor() {
+		return this.#author;
+	}
+
+	getYear() {
+		return this.#year;
+	}
+
+	getCover() {
+		return this.#cover;
+	}
+
+	getScore() {
+		return this.#score;
+	}
+
+	getIsRead() {
+		return this.#isRead;
+	}
+}
